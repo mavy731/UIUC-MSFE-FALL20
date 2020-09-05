409699dded470de0e93d4637f9a9949e634c3cd6
Yue Ma <40189991+mavy731@users.noreply.github.com>
2020-09-04 21:39:18 -0500

    Add files via upload

---
+++ Wk2/IE517_HW2_TS.py
@@ -0,0 +1,190 @@
+from sklearn import datasets
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from pydotplus import graph_from_dot_data
+from sklearn.tree import export_graphviz
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+TS=pd.read_csv('Treasury Squeeze test - DS1.csv')
+print(TS.columns)
+X=TS[['ctd1_percent','delivery_cost']].values
+y=TS['squeeze'].values
+
+print('Class labels:',np.unique(y))
+
+#Prepare dataset
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=1, stratify=y)
+
+print('Labels counts in y:', np.bincount(y))
+print('Labels counts in y_train:', np.bincount(y_train))
+print('Labels counts in y_test:', np.bincount(y_test))
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+#Plot function
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolor='black')
+
+    # highlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100,
+                    label='test set')
+
+#Building a decision tree
+
+tree_model = DecisionTreeClassifier(criterion='gini',
+                                    max_depth=4,
+                                    random_state=1)
+tree_model.fit(X_train, y_train)
+
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined,
+                      classifier=tree_model,
+                      test_idx=range(105, 150))
+
+plt.xlabel('ctd1_percent')
+plt.ylabel('delivery_cost')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.savefig('DescisionTree_TS.png', dpi=300)
+plt.show()
+
+tree.plot_tree(tree_model)
+plt.savefig('DescisionTree_TS_1.pdf')
+plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=1, stratify=y)
+
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+dot_data = export_graphviz(tree_model,
+                           filled=True,
+                           rounded=True,
+                           class_names=['Setosa',
+                                        'Versicolor',
+                                        'Virginica'],
+                           feature_names=['petal length',
+                                          'petal width'],
+                           out_file=None)
+graph = graph_from_dot_data(dot_data)
+graph.write_png('Tree_TS.png')
+
+forest = RandomForestClassifier(criterion='gini',
+                                n_estimators=25,
+                                random_state=1,
+                                n_jobs=2)
+forest.fit(X_train, y_train)
+
+plot_decision_regions(X_combined, y_combined,
+                      classifier=forest, test_idx=range(105, 150))
+
+plt.xlabel('ctd1_percent')
+plt.ylabel('delivery_cost')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.savefig('RandomForest_TS.png', dpi=300)
+plt.show()
+
+#K-Nearest Neighbors
+
+knn = KNeighborsClassifier(n_neighbors=5,
+                           p=2,
+                           metric='minkowski')
+knn.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=knn, test_idx=range(105, 150))
+
+plt.xlabel('ctd1_percent')
+plt.ylabel('delivery_cost')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.savefig('KNN_TS.png', dpi=300)
+plt.show()
+
+#Find optimized K for KNN
+k_range=range(1,26)
+scores=[]
+for k in k_range:
+    knn=KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train,y_train)
+    y_pred=knn.predict(X_test)
+    scores.append(accuracy_score(y_test,y_pred))
+
+plt.bar(k_range,scores)
+plt.xlabel('k')
+plt.ylabel('Accuracy Score')
+plt.ylim(0.5,0.6)
+plt.savefig('KNN_OptK_TS.png',dpi=300)
+plt.show()
+
+print("My name is Yue Ma")
+print("My NetID is: yuema4")
+print("I hereby certify that I have read the University policy on Academic Integrity and that I am not in violation.")
+
+
+
